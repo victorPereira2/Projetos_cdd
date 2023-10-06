1f66f56756f09623fd3745a36b21badbdabb3035
victorPereira2 <vpsilva.2@hotmail.com>
2023-10-06 16:29:08 -0300

    aula_06_10_23

---
+++ Desafio_Game/main.py
@@ -0,0 +1,45 @@
+print("Game: Pedra, Papel e Tesoura")
+
+opcoes = ["pedra", "papel", "tesoura"]
+
+while True:
+    # Pedir os nomes dos jogadores
+    nome_jogador1 = input("Digite o nome do Jogador 1: ")
+    nome_jogador2 = input("Digite o nome do Jogador 2: ")
+
+    while True:
+        escolha_jogador1 = input(f"{nome_jogador1}, escolha pedra, papel ou tesoura (ou 'sair' para encerrar): ")
+
+        if escolha_jogador1 == "sair":
+            print("Até logo!")
+            exit()
+
+        if escolha_jogador1.lower() not in opcoes:
+            print("Escolha inválida. Tente novamente.")
+            continue
+
+        escolha_jogador2 = input(f"{nome_jogador2}, escolha pedra, papel ou tesoura: ")
+
+        if escolha_jogador2.lower() not in opcoes:
+            print("Escolha inválida. Tente novamente.")
+            continue
+
+        print(f"{nome_jogador1} escolheu: {escolha_jogador1}")
+        print(f"{nome_jogador2} escolheu: {escolha_jogador2}")
+
+        if escolha_jogador1.lower() == escolha_jogador2.lower():
+            print("Empate!")
+        elif (escolha_jogador1 == "pedra" and escolha_jogador2 == "tesoura") or \
+             (escolha_jogador1 == "papel" and escolha_jogador2 == "pedra") or \
+             (escolha_jogador1 == "tesoura" and escolha_jogador2 == "papel"):
+            print(f"Parabéns {nome_jogador1}, você venceu!")
+        else:
+            print(f"Parabéns {nome_jogador2}, você venceu!")
+
+        # Perguntar se os jogadores querem jogar novamente
+        jogar_novamente = input("Quer jogar novamente? (sim/não): ").lower()
+        if jogar_novamente != "sim":
+            print("Até logo!")
+            exit()
+        else:
+            break
---
+++ pythonProject2/biblioteca.py
@@ -0,0 +1,12 @@
+def somar(n1,n2):
+    resultado_1 = n1 + n2
+    print(resultado_1)
+def subtracao(n1,n2):
+    resultado_2 = n1 - n2
+    print(resultado_2)
+def divisao(n1,n2):
+    resultado_3 = n1 / n2
+    print(resultado_3)
+def multiplicacao(n1,n2):
+    resultado_4 = n1 * n2
+    print(resultado_4)
